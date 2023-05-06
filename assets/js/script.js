9e536e785cd7cf6ea00db4b2a1f57446e0a71e51
skybreakbb <willistaylora@gmail.com>
2023-05-05 19:41:38 -0500

    added 'start quiz' button in index.html, added eventlistener in js to navigate to quiz.html on button press

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,41 @@
-var startSection = document.querySelector("#start")
-var quizSection = document.querySelector("#quiz")
-var endSection = document.querySelector("#end-screen")
+var startButton = document.querySelector("#startButton")
 
-startSection.addEventListener("click", function() {
-    document.body.children[1].children[0].setAttribute("style", "display: none");
-    document.body.children[1].children[1].setAttribute("style", "display: null");
-    document.body.children[1].children[2].setAttribute("style", "display: none");
+// eventListener for 'start quiz' button
+startButton.addEventListener("click", function() {
+    window.location.href = './quiz.html';
 })
 
-quizSection.addEventListener("click", function() {
-    document.body.children[1].children[0].setAttribute("style", "display: none");
-    document.body.children[1].children[1].setAttribute("style", "display: none");
-    document.body.children[1].children[2].setAttribute("style", "display: null");
-})
 
-endSection.addEventListener("click", function() {
-    document.body.children[1].children[0].setAttribute("style", "display: null");
-    document.body.children[1].children[1].setAttribute("style", "display: none");
-    document.body.children[1].children[2].setAttribute("style", "display: none");
-})
+
+
+
+
+
+
+
+
+
+
+
+// Old eventListener functions, no longer used
+// var startSection = document.querySelector("#start")
+// var quizSection = document.querySelector("#quiz")
+// var endSection = document.querySelector("#end-screen")
+
+// startSection.addEventListener("click", function() {
+//     document.body.children[1].children[0].setAttribute("style", "display: none");
+//     document.body.children[1].children[1].setAttribute("style", "display: null");
+//     document.body.children[1].children[2].setAttribute("style", "display: none");
+// })
+
+// quizSection.addEventListener("click", function() {
+//     document.body.children[1].children[0].setAttribute("style", "display: none");
+//     document.body.children[1].children[1].setAttribute("style", "display: none");
+//     document.body.children[1].children[2].setAttribute("style", "display: null");
+// })
+
+// endSection.addEventListener("click", function() {
+//     document.body.children[1].children[0].setAttribute("style", "display: null");
+//     document.body.children[1].children[1].setAttribute("style", "display: none");
+//     document.body.children[1].children[2].setAttribute("style", "display: none");
+// })
