8938cd3e89e78212fe8590bb2a28b12eb23d8676
skybreakbb <willistaylora@gmail.com>
2023-05-05 20:20:41 -0500

    added countdown timer before quiz starts, initiated by clicking start button. changes to quiz.html when counter reaches 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 var startButton = document.querySelector("#startButton")
+var preCountdown = document.querySelector("#countdownTimer")
+
+
+var secondsLeft = 4;
 
 // eventListener for 'start quiz' button
+//     window.location.href = "./quiz.html";     save for after countdown
 startButton.addEventListener("click", function() {
-    window.location.href = './quiz.html';
+    document.body.children[1].children[0].setAttribute("style", "display: none");
+    document.body.children[1].children[1].setAttribute("style", "display: null");
+
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        preCountdown.textContent = secondsLeft + " seconds until the quiz starts!";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            changeToQuiz();
+            preCountdown.textContent = "GO!";
+        }
+    }, 1000);
 })
 
 
-
+function changeToQuiz() {
+    window.location.href = "./quiz.html";
+}
 
 
 
