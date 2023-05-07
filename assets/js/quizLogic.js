c295c886a47d65dc2849287729cb2c0594af3e5e
skybreakbb <willistaylora@gmail.com>
2023-05-07 00:58:45 -0500

    altered function in quizLogic.js to pull question number into required parts of function corresponding with place in array

--- assets/js/quizLogic.js
+++ assets/js/quizLogic.js
@@ -1,27 +1,48 @@
 var quizQ = document.getElementById("quizQ");
 var quizOptions = document.querySelectorAll(".options");
 
-var question1 = {
-    quizQuestion: "test3 is correct",
-    options: ["test1", "test2", "test3", "test4"],
-    correctAnswer: 2,
-};
-var question2 = {
-    quizQuestion: "test1 is correct",
-    options: ["test1", "test2", "test3", "test4"],
-    correctAnswer: 0,
-};
+var question1 = [
+    {
+        quizQuestion: "test1 is correct",
+        options: ["test1", "test2", "test3", "test4"],
+        correctAnswer: 0,
+    },
+    {
+        quizQuestion: "test2 is correct",
+        options: ["test1", "test2", "test3", "test4"],
+        correctAnswer: 1,
+    },
+    {
+        quizQuestion: "test3 is correct",
+        options: ["test1", "test2", "test3", "test4"],
+        correctAnswer: 2,
+    },
+    {
+        quizQuestion: "test4 is correct",
+        options: ["test1", "test2", "test3", "test4"],
+        correctAnswer: 3,
+    },
+    {
+        quizQuestion: "test1 is correct",
+        options: ["test1", "test2", "test3", "test4"],
+        correctAnswer: 0,
+    }
+]
+
+
+
+
 
 
 
 var score = 0;
 
-function showQuestionOne() {
-    quizQ.textContent = question1.quizQuestion;
+function showQuestionOne(questionx) {
+    quizQ.textContent = questionx.quizQuestion;
     quizOptions.forEach(function(element, index) {
-        element.textContent = question1.options[index];
+        element.textContent = questionx.options[index];
         element.addEventListener("click", function() {
-            if (question1.correctAnswer == index) {
+            if (questionx.correctAnswer == index) {
                 score++;
                 console.log("correct answer");
                 console.log(score);
@@ -34,4 +55,4 @@ function showQuestionOne() {
     });
 }
 
-showQuestionOne(question1);
+showQuestionOne(question1[3]);
