e5c75c559d8fa4d9a13dccc2fefbf5a23bacf5cb
skybreakbb <willistaylora@gmail.com>
2023-05-13 18:50:02 -0500

    pulled data from local storage, parsed data, sorted data, made for loop to add li for each key value pair

--- assets/js/endscreen.js
+++ assets/js/endscreen.js
@@ -1,6 +1,33 @@
-var playAgainButton = document.getElementById("playAgainButton")
+var playAgainButton = document.getElementById("playAgainButton");
+var highScoreOl = document.getElementById("highScoreOl");
 
 playAgainButton.addEventListener("click", function(event) {
     event.preventDefault();
     window.location.href = "./index.html";
 })
+
+
+// var li1 = document.createElement("li");
+// highScoreOl.append(li1);
+
+
+var retrievedData = localStorage.getItem("scoredata");
+
+
+
+var parsedData = JSON.parse(retrievedData);
+
+parsedData.sort((a, b) => a.highscore + b.highscore);
+
+
+for (var i = 0; i < parsedData.length; i++) {
+    highScoreOl.append(document.createElement("li"));
+    highScoreOl.children[i].textContent = parsedData[i].name + parsedData[i].highscore;
+}
+
+
+// li1.textContent = parsedData[0].name + " | Score: " + parsedData[0].highscore;
+
+
+
+
